2ea0c6eb158534e6bf031d648b549873b5c07296
Sheeza Aziz <sheeza.aziz@utoronto.ca>
2021-10-07 22:56:46 -0400

    [part1] 16 sections

--- DesignCodeTut/ContentView.swift
+++ DesignCodeTut/ContentView.swift
@@ -8,14 +8,190 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var show = false
+    @State var viewState = CGSize.zero
+    @State var showCard = false
+    @State var bottomState = CGSize.zero
+    @State var showFull = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            TitleView()
+                .blur(radius: show ? 20 : 0)
+                .opacity(showCard ? 0.4 : 1)
+                .offset(y: showCard ? -200 : 0)
+                .animation(
+                    Animation
+                        .default
+                        .delay(0.1)
+                )
+            
+            BackCardView()
+                .frame(width: showCard ? 300 : 340, height: 220)
+                .background(Color("card4"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x: 0, y: show ? -400 : -40)
+                .offset(x: viewState.width, y: viewState.height)
+                .offset(y: showCard ? -180 : 0)
+                .scaleEffect(showCard ? 1 : 0.9)
+                .rotationEffect(.degrees(show ? 0 : 10))
+                .rotationEffect(.degrees(showCard ? -10 : 0))
+                .rotation3DEffect(
+                    .degrees(showCard ? 0 : 10),
+                    axis: (x: 10, y: 0, z: 0)
+                )
+                .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.5))
+            
+            BackCardView()
+                .frame(width: 340, height: 220)
+                .background(Color("card3"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x: 0, y: show ? -200 : -20)
+                .offset(x: viewState.width, y: viewState.height)
+                .offset(y: showCard ? -140 : 0)
+                .scaleEffect(showCard ? 1 : 0.95)
+                .rotationEffect(.degrees(show ? 0 : 5))
+                .rotationEffect(.degrees(showCard ? -5 : 0))
+                .rotation3DEffect(
+                    .degrees(showCard ? 0 : 5),
+                    axis: (x: 10, y: 0, z: 0)
+                )
+                .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.3))
+            
+            CardView()
+                .frame(width: showCard ? 375 : 340.0, height: 220.0)
+                .background(Color.black)
+                .clipShape(RoundedRectangle(cornerRadius: showCard ? 30 : 20, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/))
+                .shadow(radius: 20)
+                .offset(x: viewState.width, y: viewState.height)
+                .offset(y: showCard ? -100 : 0)
+                .blendMode(.hardLight)
+                .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0))
+                .onTapGesture {
+                    showCard.toggle()
+                }
+                .gesture(
+                    DragGesture().onChanged { value in
+                        viewState = value.translation
+                        show = true
+                    }
+                    .onEnded { value in
+                        viewState = .zero
+                        show = false
+                    }
+                )
+                        
+            BottomCardView()
+                .offset(x: 0, y: showCard ? 360 : 1000)
+                .offset(y: bottomState.height)
+                .blur(radius: show ? 20 : 0)
+                .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
+                .gesture(
+                    DragGesture().onChanged { value in
+                        bottomState = value.translation
+                        if showFull {
+                            bottomState.height += -300
+                        }
+                        if bottomState.height < -300 {
+                            bottomState.height = -300
+                        }
+                    }
+                    .onEnded { value in
+                        if bottomState.height > 50 {
+                            showCard = false
+                        }
+                        if (bottomState.height < -100 && !showFull) || (bottomState.height < -250 && showFull ) {
+                            bottomState.height = -300
+                            showFull = true
+                        }
+                        else {
+                            showCard = false
+                            showFull = false
+                        }
+                    }
+                )
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("UI Design")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                    Text("Certificate")
+                        .foregroundColor(Color("accent"))
+                }
+                Spacer()
+                Image("Logo1")
+            }
+            .padding(.horizontal, 20)
+            .padding(.top, 20)
+            Spacer()
+            Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 300, height: 110, alignment: .top)
+        }
+    }
+}
+
+struct BackCardView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+        }
+    }
+}
+
+struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Certificates")
+                    .font(.largeTitle)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                Spacer()
+            }
+            .padding()
+            Image("Background1")
+            Spacer()
+        }
+    }
+}
+
+struct BottomCardView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Rectangle()
+                .frame(width: 40, height: 5)
+                .cornerRadius(3)
+                .opacity(0.1)
+            Text("This certificate is proof that Sheeza Aziz has achieved great design skills.")
+                .multilineTextAlignment(.center)
+                .font(.subheadline)
+                .lineSpacing(4)
+            Spacer()
+        }
+        .padding(.top, 8)
+        .padding(.horizontal, 20)
+        .frame(width: .infinity)
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(radius: 20)
     }
 }
---
+++ DesignCodeTut/HomeView.swift
@@ -0,0 +1,95 @@
+//
+//  HomeView.swift
+//  DesignCodeTut
+//
+//  Created by Sheeza Aziz on 2021-10-07.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @Binding var showProfile: Bool
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Watching")
+                    .font(.system(size: 28, weight: .bold))
+                
+                Spacer()
+                
+                AvatarView(showProfile: $showProfile)
+            }
+            .padding(.horizontal)
+            .padding(.leading, 14)
+            .padding(.top, 30)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 20) {
+                    ForEach(sectionData) { item in
+                        GeometryReader { geometry in
+                            SectionView(section: item)
+                                .rotation3DEffect(.degrees(Double(geometry.frame(in: .global).minX - 30) / -20), axis: (x: 0, y: 10, z: 0))
+                        }
+                        .frame(width: 275, height: 275)
+                    }
+                }
+                .padding(30)
+                .padding(.bottom, 30)
+            }
+            
+            Spacer()
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView(showProfile: .constant(false))
+    }
+}
+
+struct SectionView: View {
+    var section: Section
+    
+    var body: some View {
+        VStack {
+            HStack(alignment: .top) {
+                Text(section.title)
+                    .font(.system(size: 24, weight: .bold))
+                    .frame(width: 160, alignment: .leading)
+                    .foregroundColor(.white)
+                Spacer()
+                Image(section.logo)
+            }
+            Text(section.text.uppercased())
+                .frame(maxWidth: .infinity, alignment: .leading)
+            section.image
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 210)
+        }
+        .padding(.top, 20)
+        .padding(.horizontal, 20)
+        .frame(width: 275, height: 275)
+        .background(section.color)
+        .cornerRadius(30)
+        .shadow(color: section.color.opacity(0.3), radius: 20, x: 0.0, y: 20)
+    }
+}
+
+struct Section: Identifiable {
+    var id = UUID()
+    var title: String
+    var text: String
+    var logo: String
+    var image: Image
+    var color: Color
+}
+
+let sectionData = [
+    Section(title: "Prototype designs in SwiftUI", text: "18 Sections", logo: "Logo1", image: Image("Card1"), color: Color("card1")),
+    Section(title: "Build a SwiftUI app", text: "20 Sections", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Card4")), color: Color(#colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1))),
+    Section(title: "Advanced SwiftUI", text: "24 Sections", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Card3")), color: Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1))),
+]
+
